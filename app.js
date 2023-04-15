2cde025ee41cbd0a0cfd69f049ef4168bf221498
uelker <enes.uelker@hotmail.com>
2023-04-15 16:45:20 +0200

    chore: using logger instead of console.log

--- app.js
+++ app.js
@@ -3,14 +3,16 @@
 const http = require('http');
 const getApp = require('./lib/getApp');
 const database = require('./lib/database');
+const loggingConfig = require('./lib/logging-configuration');
+const logger = require('pino')(loggingConfig);
 
 const connectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017/admin';
 const port = process.env.PORT || 3_000;
 
 database.initialize(connectionString, (err) => {
     if(err) {
-        console.log('Error connecting to database');
-        console.log(err);
+        logger.info('Error connecting to database');
+        logger.info(err);
         process.exit(1);
     }
 
@@ -18,7 +20,7 @@ database.initialize(connectionString, (err) => {
     const server = http.createServer(app);
 
     server.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
+        logger.info(`Server listening on port ${port}`);
     });
 });
 
