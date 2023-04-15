c7d9335e3a7c663211683caa8e9d4573b48aa4fa
uelker <enes.uelker@hotmail.com>
2023-04-15 14:30:23 +0200

    feat: using a mongodb database

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 
 const http = require('http');
 const getApp = require('./lib/getApp');
+const database = require('./lib/database');
 
-
+const connectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017/admin';
 const port = process.env.PORT || 3_000;
-const app   = getApp();
-const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+database.initialize(connectionString, (err) => {
+    if(err) {
+        console.log('Error connecting to database');
+        console.log(err);
+        process.exit(1);
+    }
+
+    const app   = getApp();
+    const server = http.createServer(app);
+
+    server.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
 });
+
